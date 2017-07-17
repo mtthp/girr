b92e0f5c4f570752561eb988f2c18a2afadc1aae
Christophe Camicas <christophe.camicas@gmail.com>
2017-07-17 13:29:56 +0200

    réintégration style GeekInc closes #1

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ app.use(express.static('./public')) // Indique que le dossier /public contient d
   });
 
 io.on('connection', socket => {
+
   socket.emit('setTitle', { text: 'Title ready !', timeout: 5 });
+
   socket.on('setTitle', data => {
     console.log(data);
     io.sockets.emit('setTitle', data);
@@ -19,10 +21,15 @@ io.on('connection', socket => {
     //todo check the file exists in the directory
     io.sockets.emit('setIncrust', data);
   });
+
   socket.on('clearIncrust', data => {
     io.sockets.emit('clearIncrust', data);
   });
 
 });
 
-io.listen(app.listen(8081));
+let port = 8081;
+io.listen(app.listen(port, () => {
+  console.log(`http://localhost:${port}/admin.html`);
+  console.log(`http://localhost:${port}/xsplit.html`);
+}));
