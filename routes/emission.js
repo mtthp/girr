e3a81136a5ae8e8df4442ff5e32b938c001a2f6e
chriscamicas <christophe.camicas@gmail.com>
2017-07-21 23:39:28 +0200

    début gestion upload image

--- routes/emission.js
+++ routes/emission.js
@@ -2,20 +2,41 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const router = express.Router();
+const fileUpload = require('express-fileupload');
+
+router.use('/:emission/:episode', (req, res, next) => {
+    req.publicPathToIncrusts = `assets/${req.params.emission}/${req.params.episode}`;
+    req.localPathToIncrusts = path.join('public', req.publicPathToIncrusts);
+    next();
+});
 
 router.get('/:emission/:episode/incrusts', (req, res) => {
     let incrusts = [];
-    let publicPathToIncrusts = `assets/${req.params.emission}/${req.params.episode}`;
-    let localPathToIncrusts = path.join('public', publicPathToIncrusts);
-    fs.readdir(localPathToIncrusts, (err, files) => {
+
+    fs.readdir(req.localPathToIncrusts, (err, files) => {
         if(err) {
             return res.status(404).send(err);
         }
         if(files) {
-            incrusts = files.map(f => path.join(publicPathToIncrusts, f));
+            incrusts = files.map(f => path.join(req.publicPathToIncrusts, f));
         }
         return res.send(incrusts);
     });
+});
+
+router.use(fileUpload());
+
+router.post('/:emission/:episode/incrusts', (req, res) => {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+
+  let incrustFile = req.files.incrust;
+
+  incrustFile.mv(path.join(req.localPathToIncrusts, req.files.incrust.name), err => {
+    if (err)
+      return res.status(500).send(err);
+    res.send('File uploaded!');
+  });
 });
 
 module.exports = router;
