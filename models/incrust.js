73e6c53290ac57b8f215063c1f0c1f99b8141b6c
Matthieu PETIT <m4tth.p@gmail.com>
2017-09-18 18:22:19 +0200

    [#] Refactor News and Incrusts models

--- models/incrust.js
+++ models/incrust.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Incrust', new mongoose.Schema({
-    news: { type: mongoose.Schema.Types.ObjectId, ref:'News' },
+module.exports = mongoose.model('Incrust',
+  new mongoose.Schema({
     data: Buffer,
-    contentType: String
-}));
+    contentType: String,
+    news: { type: mongoose.Schema.Types.ObjectId, ref:'News', required: true },
+  })
+);
--- models/news.js
+++ models/news.js
@@ -1,9 +1,26 @@
+"use strict";
 const mongoose = require('mongoose');
+const Incrust = require('./incrust');
+const logger = require('../logger');
 
-module.exports = mongoose.model('News', new mongoose.Schema({
+let newsSchema = new mongoose.Schema({
     numero: { type: Number, required: true },
     titre: { type: String },
     notes: String,
     episode: { type: mongoose.Schema.Types.ObjectId, ref:'Episode' },
     incrusts: [{ type: mongoose.Schema.Types.ObjectId, ref:'Incrust' }]
-}));
+});
+
+// when a News is removed, delete all its Incrusts
+newsSchema.post('remove', function(news) {
+  Incrust
+    .remove({news: news._id})
+    .then(function(result) {
+      logger.debug("Removed all Incrust from News " + news._id)
+    })
+    .catch(function(error) {
+      logger.error(error)
+    })
+})
+
+module.exports = mongoose.model('News', newsSchema);
