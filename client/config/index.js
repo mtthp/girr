ac5c5c808544e6c6a3f11d1c9dd85fce9be3a47a
Matthieu PETIT <m4tth.p@gmail.com>
2017-10-06 18:27:09 +0200

    [#] Use proxyTable from webpack instead of an env var

--- client/config/index.js
+++ client/config/index.js
@@ -27,7 +27,23 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // proxy all requests starting with /api to the server address
+      '/api': {
+        target: 'http://localhost:8080/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/data': {
+        target: 'http://localhost:8080/data',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/data': ''
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
