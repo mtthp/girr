4f1cffac671b984a056089984957eef444d320ef
Matthieu PETIT <m4tth.p@gmail.com>
2017-10-13 17:02:04 +0200

    [#] Specify which logging levels we use

--- server/src/logger.js
+++ server/src/logger.js
@@ -3,6 +3,14 @@ var path = require('path');
 winston.emitErrs = true;
 
 var logger = new winston.Logger({
+    levels: {
+        trace: 0,
+        debug: 1,
+        info: 2,
+        warn: 3,
+        error: 4,
+        critical: 5
+    },
     transports: [
         new winston.transports.File({
             name: 'info-file',
