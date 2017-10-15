88ef7ec0c349fa303666b810c8743062c6dbdc91
Matthieu PETIT <m4tth.p@gmail.com>
2017-10-15 15:27:51 +0200

    [#] Allow labeled loop

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -22,6 +22,11 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // labeled loop to break or continue outer loop
+    'no-labels': [2, {
+      'allowLoop': true,
+      'allowSwitch': false
+    }]
   }
 }
