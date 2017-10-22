11a4ad6223773079566db7fded04b91bcc85214b
Matthieu PETIT <m4tth.p@gmail.com>
2017-10-22 20:31:59 +0200

    [Fix] Websocket server URI is defined as an env variable

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,7 @@ import VueResource from 'vue-resource'
 import VueSocketio from 'vue-socket.io'
 
 Vue.use(VueResource)
-Vue.use(VueSocketio, 'http://localhost:8080')
+Vue.use(VueSocketio, process.env.WEBSOCKET_URI)
 
 Vue.config.productionTip = false
 
