7fb862010a01e2861c65babd26b9576f83fc6a72
Matthieu PETIT <m4tth.p@gmail.com>
2017-10-24 20:55:29 +0200

    [+] Add GeekInc logo to the drawer header

--- client/src/components/Drawer.vue
+++ client/src/components/Drawer.vue
@@ -1,25 +1,26 @@
 <template>
   <aside class="mdc-temporary-drawer mdc-typography">
     <nav class="mdc-temporary-drawer__drawer">
-        <header class="mdc-temporary-drawer__header">
-            <div class="mdc-temporary-drawer__header-content">
-                GeekInc Remote Regie
-            </div>
-        </header>
-        <nav id="icon-with-text-demo" class="mdc-temporary-drawer__content mdc-list">
-          <router-link :to="{ name: 'Programs' }" @click.native="toggleDrawer(false)" class="mdc-list-item" active-class="mdc-temporary-drawer--selected" data-mdc-auto-init="MDCRipple">
-            <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">event</i>Programs
-          </router-link>
-          <router-link :to="{ name: 'Xsplit' }" @click.native="toggleDrawer(false)" class="mdc-list-item" active-class="mdc-temporary-drawer--selected" data-mdc-auto-init="MDCRipple">
-            <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">tv</i>Xsplit
-          </router-link>
-          <router-link :to="{ name: 'Admin' }" @click.native="toggleDrawer(false)" class="mdc-list-item" active-class="mdc-temporary-drawer--selected" data-mdc-auto-init="MDCRipple">
-            <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">build</i>Admin
-          </router-link>
-          <a class="mdc-list-item" href="/api/" target="_blank" data-mdc-auto-init="MDCRipple">
-            <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">code</i>API
-          </a>
-        </nav>
+      <header class="mdc-temporary-drawer__header">
+        <div class="mdc-temporary-drawer__header-content mdc-theme--primary-bg mdc-theme--text-primary-on-primary">
+          <div>GeekInc Remote Regie</div>
+          <img :src="require('../assets/geekinc-logo_512.png')"/>
+        </div>
+      </header>
+      <nav id="icon-with-text-demo" class="mdc-temporary-drawer__content mdc-list">
+        <router-link :to="{ name: 'Programs' }" @click.native="toggleDrawer(false)" class="mdc-list-item" active-class="mdc-temporary-drawer--selected" data-mdc-auto-init="MDCRipple">
+          <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">event</i>Programs
+        </router-link>
+        <router-link :to="{ name: 'Xsplit' }" @click.native="toggleDrawer(false)" class="mdc-list-item" active-class="mdc-temporary-drawer--selected" data-mdc-auto-init="MDCRipple">
+          <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">tv</i>Xsplit
+        </router-link>
+        <router-link :to="{ name: 'Admin' }" @click.native="toggleDrawer(false)" class="mdc-list-item" active-class="mdc-temporary-drawer--selected" data-mdc-auto-init="MDCRipple">
+          <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">build</i>Admin
+        </router-link>
+        <a class="mdc-list-item" href="/api/" target="_blank" data-mdc-auto-init="MDCRipple">
+          <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">code</i>API
+        </a>
+      </nav>
     </nav>
   </aside>
 </template>
@@ -48,6 +49,25 @@ export default {
 
 <style scoped>
 .mdc-temporary-drawer__header-content {
-  color: black;
+  flex-flow: wrap-reverse;
+  background-color: rgba(128, 128, 128, 0.4);
+}
+
+.mdc-temporary-drawer__header-content p {
+  align-self: flex-start; /* sized to content */
+  flex: 1 0 auto; /* grow to content */
+}
+
+.mdc-temporary-drawer__header-content img {
+  flex: 1 1 100%; /* fills remaining space */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  object-fit: contain;
+  filter: drop-shadow(0px 0px 1px rgba(0,0,0,.3))
+          drop-shadow(0px 0px 10px rgba(0,0,0,.3))
+          drop-shadow(0px 0px 20px rgba(0,0,0,.3));
 }
 </style>
